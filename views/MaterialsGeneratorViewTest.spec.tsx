3a71043042257611ff5cf28b7789372f70b2d8cf
Igor Bogdanoski <bogdanoskiigor@oublazekoneski.edu.mk>
2026-03-07 18:03:53 +0100

    chore: cleanup old error logs and dummy tests

--- views/MaterialsGeneratorViewTest.spec.tsx
+++
@@ -1,2 +0,0 @@
-﻿import { describe, it } from 'vitest';
-import { render } from '@testing-library/react'; import React from 'react'; import { MaterialsGeneratorView } from '../views/MaterialsGeneratorView'; describe('MaterialsGeneratorView', () => { it('renders without crashing', () => { render(<MaterialsGeneratorView />); }); });
